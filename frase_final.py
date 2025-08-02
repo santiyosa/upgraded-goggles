6b2f279442b5f4df62dae31b95c21912372a4564
sachi <ardyosa@hotmail.com>
2025-08-02 11:51:22 -0500

    feat: implementación del proyecto base

---
+++ frase_final.py
@@ -0,0 +1,21 @@
+# El programa solo funcionará una vez que todas las variables estén completas y fusionadas.
+from random import choice
+# El líder del equipo añade su parte (la primera palabra de la frase)
+parte_1 = "El gato"
+# Aquí el integrante 2 debe completar el ejercicio
+parte_2 = ""
+# Aquí el integrante 3 debe completar el ejercicio
+parte_3 = ""
+# Aquí el integrante 4 debe completar el ejercicio
+parte_4 = ""
+# Aquí el integrante 5 debe completar el ejercicio
+parte_5 = ""
+# Aquí el integrante 6 debe completar el ejercicio
+parte_6 = ""
+# La frase completa se genera concatenando las variables
+parte_7 = ""
+# La frase completa se genera concatenando las variables
+parte_8 = ""
+# La frase completa se genera concatenando las variables
+frase_final = f"{parte_1} {parte_2} {parte_3} {parte_4} {parte_5} {parte_6} {parte_7} {parte_8}."
+print(frase_final)
