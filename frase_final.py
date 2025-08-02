6481349c4e4c59b9a414edb3275c57bea717b18c
JessiG24 <jessica98jgz@gmail.com>
2025-08-02 14:11:29 -0500

    Feat: parte 2.1

--- frase_final.py
+++ frase_final.py
@@ -1,9 +1,9 @@
 # El programa solo funcionará una vez que todas las variables estén completas y fusionadas.
 from random import choice
 # El líder del equipo añade su parte (la primera palabra de la frase)
-parte_1 = "Otro gatog"
+parte_1 = "El gato"
 # Aquí el integrante 2 debe completar el ejercicio
-parte_2 = "Soy el gato"
+parte_2 = "Soy el gato Pyth, mucho gusto."
 # Aquí el integrante 3 debe completar el ejercicio
 parte_3 = ""
 # Aquí el integrante 4 debe completar el ejercicio
@@ -15,7 +15,7 @@ parte_6 = ""
 # La frase completa se genera concatenando las variables
 parte_7 = ""
 # La frase completa se genera concatenando las variables
-parte_8 = "Rojo Carmesí"
+parte_8 = ""
 # La frase completa se genera concatenando las variables
 frase_final = f"{parte_1} {parte_2} {parte_3} {parte_4} {parte_5} {parte_6} {parte_7} {parte_8}."
 print(frase_final)
